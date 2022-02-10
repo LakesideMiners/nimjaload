9484cae4ccb34315d844bf9a1ce754775ec4daaf
LakesideMiners <23389169+LakesideMiners@users.noreply.github.com>
2022-02-10 08:30:26 -0500

    commit

--- main.py
+++ main.py
@@ -14,12 +14,57 @@ cache_time = config['control']['cacheTime']
 api_url = "https://hypno.nimja.com/app"
 
 
+def checkCache():
+    """Checks to see if our stored copy is more then
+    """    
+    # Check first if the file exists, if it does, we check when it was downloaded
+    if exists("data/app.json"):
+        f = open("data/app.json")
+        data = json.load(f)
+        now = datetime.now()    
+        nowts = now.timestamp()
+        last_updated = data['time']
+        print(last_updated)
+        difference = float(nowts) - float(last_updated)
+        print("app.json last updated " + str(difference)  + " seconds ago!") 
+        if difference >= float(cache_time):
+            print("Older then " + cache_time + " seconds! Updating!")
+            os.remove("data/app.json")
+            getApp()
+        else:
+            print("Not older then " + cache_time + " seconds, so we won't update!")
+    else:
+        print("We don't have app.json! So we are getting it")
+        getApp()
 
+# Gets the app
+def getApp():
+    r = requests.get(api_url)
+    with open("data/app.json", "w") as f:
+        f.write(r.text)
+        print("Done Updating")
+        f.close()
         
-# TODO: Ask user for cookies and check if expired.
-#def formatCookies(): 
+        
+# Only runs if user is not a supporter
+def formatCookies(): 
+    persist_input = input("Please enter the persist cookie: \n")
+    persist = {'persist': persist_input}
+    print(persist)
+    r = requests.get("https://hypno.nimja.com/listen/download/339-loyal_reward.mp3", cookies=persist)
+    if r.status_code == requests.codes.ok:
+        print("Cookie is VALID! Continuing!")
+        return persist
+
+    else:
+        print(r.status_code)
+        print("Cookie invalid. try again")
+        formatCookies()
+        
+        
         
 
+# Downloaders
 #use if not a patreon
 def normalDL(file_ids):
     f = open("data/app.json")
@@ -62,46 +107,16 @@ def patreonDL(cookies, file_ids):
     print("Playlist download done!")
                 
   
-# TODO: Setup Caching and Download app.json         
-def checkCache():
-    """Checks to see if our stored copy is more then
-    """    
-    # Check first if the file exists, if it does, we check when it was downloaded
-    if exists("data/app.json"):
-        f = open("data/app.json")
-        data = json.load(f)
-        now = datetime.now()    
-        nowts = now.timestamp()
-        last_updated = data['time']
-        print(last_updated)
-        difference = float(nowts) - float(last_updated)
-        print("app.json last updated " + str(difference)  + " seconds ago!") 
-        if difference >= float(cache_time):
-            print("Older then " + cache_time + " seconds! Updating!")
-            os.remove("data/app.json")
-            getApp()
-        else:
-            print("Not older then " + cache_time + " seconds, so we won't update!")
-    else:
-        print("We don't have app.json! So we are getting it")
-        getApp()
 
 
-def getApp():
-    r = requests.get(api_url)
-    with open("data/app.json", "w") as f:
-        f.write(r.text)
-        print("Done Updating")
-        f.close()
-        
+
+
 playlist_url = input("Please enter the playlist URL: \n")
 playlist_striped = playlist_url.rsplit('/', 1)[-1]
 file_ids = playlist_striped.split("-")
 
 
 if patron == "True":
-
-
     checkCache()
     patreonDL(formatCookies(), file_ids)
 else:
